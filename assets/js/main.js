5503b66e7c4d839fad5d813d20b20b7a8a1d8687
guilherme amarilho <guiamarilho1@outlook.com>
2022-08-21 04:03:54 -0300

    scrolls trabalhados

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,12 +37,33 @@ const scrollHeader = () =>{
 }
 window.addEventListener('scroll', scrollHeader)
 
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
@@ -52,7 +73,6 @@ const calculateForm = document.getElementById('calculate-form'),
  calculateCm = document.getElementById('calculate-cm'),
  calculateKg = document.getElementById('calculate-kg'),
  calculateMessage = document.getElementById('calculate-message');
-
 const calculateIMC = (e) => {
     e.preventDefault();
     // Testa se foram preenchidos os input
@@ -93,7 +113,6 @@ calculateForm.addEventListener('submit', calculateIMC);
 const contactForm = document.getElementById('contact-form'),
  contactMessage = document.getElementById('contact-message'),
  contactUser = document.getElementById('contact-user');
-
 const sendEmail = (e) =>{
     e.preventDefault();
     // Checa de o input foi preenchido
@@ -117,5 +136,4 @@ const sendEmail = (e) =>{
         contactUser.value='';
     }
 } 
-
 contactForm.addEventListener('submit', sendEmail);
